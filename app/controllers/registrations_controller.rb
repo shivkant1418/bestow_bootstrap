3cc20dfc575a108dc74a21e34737544398328662
Shivkant <dohaliyashivkant@gmail.com>
2018-09-15 10:09:07 +0530

    Fixed devise update params

--- app/controllers/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, :only => [:create]
-
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :contact) }
-    end
-end
